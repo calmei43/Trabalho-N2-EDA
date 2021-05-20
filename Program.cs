0c057038ab05615f6a2ddc72d2873728b188e1cf
calmei43 <calmeid42@gmail.com>
2021-05-19 22:50:08 -0300

    V0.0

--- Program.cs
+++ Program.cs
@@ -5,25 +5,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Threading;
 
 namespace EDA_N2B2
 {
     class Program
     {
+        static void LeRegistroCategoria()
+        {
+            RegistroCategoria registroCat = new RegistroCategoria("categorias.txt");
+        }
+
+        static void LeRegistroCliente()
+        {
+            RegistroCliente registroCliente = new RegistroCliente("clientes.txt");
+        }
+
+        static void LeRegistroProduto()
+        {
+            RegistroProduto registroProduto = new RegistroProduto("produtos.txt");
+        }
+
+        //static void LeRegistroVenda()
+        //{
+        //    RegistroVenda registroVenda = new RegistroVenda("vendas.txt", registroCliente.GetContent(), registroProduto.GetContent());
+        //}
+
         static void Main(string[] args)
         {
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-
-            RegistroCategoria registroCat = new RegistroCategoria("categorias.txt");
             RegistroCliente registroCliente = new RegistroCliente("clientes.txt");
             RegistroProduto registroProduto = new RegistroProduto("produtos.txt");
+
+            Thread[] vetorThread = new Thread[3];
+
+            vetorThread[0] = new Thread(new ThreadStart(LeRegistroCategoria));
+            vetorThread[1] = new Thread(new ThreadStart(LeRegistroCliente));
+            vetorThread[2] = new Thread(new ThreadStart(LeRegistroProduto));
+           
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
             RegistroVenda registroVenda = new RegistroVenda("vendas.txt", registroCliente.GetContent(), registroProduto.GetContent());
 
-            Console.WriteLine(registroCat); 
-            Console.WriteLine(registroCliente); 
-            Console.WriteLine(registroProduto); 
-            Console.WriteLine(registroVenda); 
+            foreach (var T in vetorThread)
+            {
+                T.Start();
+            }
 
 
             sw.Stop();
